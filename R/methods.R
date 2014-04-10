8f1cbe84fe246c0eb9c3cd7c6150ab8dd6d44b1a
jaredhuling <huling@wisc.edu>
2014-04-09 20:47:10 -0500

    fixed printing error

--- R/methods.R
+++ R/methods.R
@@ -15,18 +15,20 @@ predict.owlfit <- function(object, type.measure = c("class", "value", "aic", "de
 print.owlfit <- function(obj) {
   cat("\nCall: ", deparse(obj$call), "\n\n")
   
-  lams <- data.frame(array(0, dim = c(4, 2)))
-  rownames(lams) <- c("Optimal", "Class", "Value", "AIC")
-  colnames(lams) <- c("Lambda", "Pct Correct")
-  lams[1,1] <- obj$optimal.lambda
-  lams[1,2] <- obj$max.pct.correct
-  lams[2,1] <- obj$class.lambda
-  lams[2,2] <- obj$class.pct.correct
-  lams[3,1] <- obj$value.lambda
-  lams[3,2] <- obj$value.pct.correct
-  lams[4,1] <- obj$aic.lambda
-  lams[4,2] <- obj$aic.pct.correct
-  print(lams)
+  if (!is.null(max.pct.correct)) {
+    lams <- data.frame(array(0, dim = c(4, 2)))
+    rownames(lams) <- c("Optimal", "Class", "Value", "AIC")
+    colnames(lams) <- c("Lambda", "Pct Correct")
+    lams[1,1] <- obj$optimal.lambda
+    lams[1,2] <- obj$max.pct.correct
+    lams[2,1] <- obj$class.lambda
+    lams[2,2] <- obj$class.pct.correct
+    lams[3,1] <- obj$value.lambda
+    lams[3,2] <- obj$value.pct.correct
+    lams[4,1] <- obj$aic.lambda
+    lams[4,2] <- obj$aic.pct.correct
+    print(lams)
+  }
   
   cat("\n")
   
