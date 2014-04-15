8da9c626b6f3129e1169eae0c18805fe576299e2
jaredhuling <huling@wisc.edu>
2014-04-15 12:27:17 -0500

    updated plot method

--- R/methods.R
+++ R/methods.R
@@ -49,14 +49,47 @@ print.owlfit <- function(obj) {
 
 
 plot.owlfit <- function(obj) {
-  K <- nrow(obj$d.vals)
-  nonan <- obj$d.vals; nonan[is.nan(nonan)] <- mean(nonan[!is.nan(nonan)])
-  ylims <- c(min(na.omit(obj$d.vals))-0.25 * sd(nonan[!is.nan(nonan)]), max(na.omit(obj$d.vals)+0.25 * sd(nonan[!is.nan(nonan)])))
-  cols <- c("red", "green", "yellow", "purple", "orange", colors()[sample.int(400, K)])
-  plot(x = 1:length(obj$model$lambda), y = obj$d.vals[1,], type = "l", col = "blue", ylim = ylims)
-  for (i in 2:K) {
-    lines(x = 1:length(obj$model$lambda), y = obj$d.vals[i,], col = cols[i-1])
+  #K <- nrow(obj$d.vals)
+  #nonan <- obj$d.vals; nonan[is.nan(nonan)] <- mean(nonan[!is.nan(nonan)])
+  #ylims <- c(min(na.omit(obj$d.vals))-0.25 * sd(nonan[!is.nan(nonan)]), max(na.omit(obj$d.vals)+0.25 * sd(nonan[!is.nan(nonan)])))
+  #cols <- c("red", "green", "yellow", "purple", "orange", colors()[sample.int(400, K)])
+  #plot(x = 1:length(obj$model$lambda), y = obj$d.vals[1,], type = "l", col = "blue", ylim = ylims)
+  #for (i in 2:K) {
+  #  lines(x = 1:length(obj$model$lambda), y = obj$d.vals[i,], col = cols[i-1])
+  #}
+  #print(ylims)
+  require(grid)
+  require(gridExtra)
+  
+  n.trt <- nrow(mod21$d.vals)
+  n.lam <- ncol(mod21$d.vals)
+  d.vals <- numeric(length = (n.trt) * n.lam)
+  for (i in 1:(nrow(mod21$d.vals))) {
+    d.vals[((i-1) * n.lam + 1):(i * n.lam)] <- mod21$d.vals[i,]
   }
-  print(ylims)
+  
+  dfs <- if(inherits(obj$model, "msgl")) {df.msgl(obj)} else {obj$model$df}
+  
+  dvaldat <- data.frame(dvals = d.vals, 
+                        treatment = rep(as.character(1:4), each = ncol(obj$d.vals)),
+                        df = rep(dfs, nrow(obj$d.vals)),
+                        lambda = rep(obj$model$lambda, nrow(obj$d.vals)))
+  
+  dfdat <- data.frame(df = rep(dfs, nrow(obj$d.vals)),
+                      lambda = rep(obj$model$lambda, nrow(obj$d.vals)))
+  
+  p1 <- ggplot(aes(x = -lambda, y = dvals, color = treatment), data = dvaldat) + geom_line(size=1.2) + 
+    theme_bw() + theme(legend.position = "bottom") + geom_vline(xintercept = -obj$class.lambda) +
+    geom_vline(xintercept = -obj$aic.lambda)
+  
+  p2 <- ggplot(aes(x = -lambda, y = df), data = dfdat) + geom_line(size=1.2) + theme_bw() +
+    geom_vline(xintercept = -obj$class.lambda) +
+    geom_text(aes(x = -obj$class.lambda, label="\n Misclassification Selection", y = (obj$model$df[obj$class.lambda.idx] + max(obj$model$df)) / 2.5), 
+              colour="blue", angle=90, text=element_text(size=11)) +
+    geom_vline(xintercept = -obj$aic.lambda) +
+    geom_text(aes(x = -obj$aic.lambda, label="\n AIC Selection", y = (obj$model$df[obj$aic.lambda.idx] + max(obj$model$df)) / 2.5), 
+              colour="blue", angle=90, text=element_text(size=11))
+  print(grid.draw(rbind(ggplotGrob(p1), ggplotGrob(p2), size = "last")))
+  list(p1 = p1, p2 = p2)
 }
 
